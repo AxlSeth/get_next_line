72b0f5e2fb80e6640d5fdd7b016fc205cf6fd042
Axel <seramaro@student.42antananarivo.mg>
2026-03-24 01:23:39 +0300

    fory

--- get_next_line.c
+++ get_next_line.c
@@ -3,35 +3,197 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seramaro <seramaro@student.42antananari    +#+  +:+       +#+        */
+/*   By: seramaro <seramaro@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/10 22:15:17 by seramaro          #+#    #+#             */
-/*   Updated: 2026/03/17 14:05:47 by seramaro         ###   ########.fr       */
+/*   Created: 2026/03/23 10:32:35 by seramaro          #+#    #+#             */
+/*   Updated: 2026/03/24 01:10:57 by seramaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd, int buffer_size)
+int	ft_strlen(char *str)
 {
-	char		*line;
-	char		*buffer;
-	static char	*stash;
-	int			i;
+	int	i;
 
 	i = 0;
-	buffer = malloc(sizeof(char) * buffer_size);
-	read(fd, buffer, buffer_size);
-	
-	while(stash[i])
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	has_new_line(char *stash)
+{
+	int	i;
+
+	if (!stash)
+		return (0);
+	i = 0;
+	while (stash[i])
 	{
-		if (stash[i] != '\n')
-			stash = ft_strjoin(stash, buffer);
-		else if (stash[i] == '\n')
-		{
-			line = malloc(sizeof(char) * i);
-		}
+		if (stash[i] == '\n')
+			return (1);
 		i++;
 	}
+	return (0);
+}
+
+char	*append(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (str);
+}
+
+char	*create_buffer(int fd)
+{
+	char	*buffer;
+	int		end;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	end = read(fd, buffer, BUFFER_SIZE);
+	if (end == 0)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	else if (end == -1)
+		return (NULL);
+	buffer[end] = '\0';
+	return (buffer);
+}
+
+int	line_size(char *stash)
+{
+	int	position;
+
+	position = 0;
+	while (stash[position])
+	{
+		if (stash[position] == '\n')
+			return (position + 1);
+		position++;
+	}
+	if (has_new_line(stash))
+		return (position + 1);
+	else
+		return (position);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t l)
+{
+	unsigned char	*char_dest;
+	unsigned char	*char_src;
+
+	char_dest = (unsigned char *)dest;
+	char_src = (unsigned char *)src;
+	if (char_dest < char_src)
+	{
+		while (l--)
+			*char_dest++ = *char_src++;
+	}
+	else if (char_dest > char_src)
+	{
+		while (l--)
+			char_dest[l] = char_src[l];
+	}
+	return (dest);
+}
+
+char	*create_line(char *stash)
+{
+	char	*line;
+
+	line = malloc(sizeof(char) * (line_size(stash) + 1));
+	line = (char *)ft_memmove(line, stash, (line_size(stash)));
+	line[line_size(stash)] = '\0';
 	return (line);
+}
+
+char	*store_remainder(char *stash)
+{
+	int		len;
+	char	*remainder;
+
+	len = ft_strlen(stash) - line_size(stash);
+	remainder = malloc(sizeof(char) * (len + 1));
+	remainder = ft_memmove(remainder, (stash + line_size(stash)), len);
+	remainder[len] = '\0';
+	return (remainder);
+}
+
+void	update_line(char **stash, char **remainder, char **line)
+{
+	if (has_new_line(*stash))
+	{
+		*line = create_line(*stash);
+		*remainder = store_remainder(*stash);
+		free(*stash);
+		*stash = *remainder;
+	}
+	else if (*stash != NULL)
+	{
+		*line = create_line(*stash);
+		free(*stash);
+		*stash = NULL;
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*line;
+	char		*remainder;
+	char		*old_stash;
+	char		*buf;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (stash == NULL)
+		stash = create_buffer(fd);
+	while (!has_new_line(stash))
+	{
+		old_stash = stash;
+		buf = create_buffer(fd);
+		if (!buf)
+			break ;
+		stash = append(old_stash, buf);
+		free(old_stash);
+		free(buf);
+	}
+	update_line(&stash, &remainder, &line);
+	return (line);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*line1;
+	char	*line2;
+	char	*line3;
+
+	fd = open("nigga.txt", O_RDONLY);
+	line1 = get_next_line(fd);
+	line2 = get_next_line(fd);
+	line3 = get_next_line(fd);
+	printf("%s%s%s", line1, line2, line3);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,19 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: seramaro <seramaro@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/09 15:02:59 by seramaro          #+#    #+#             */
-/*   Updated: 2026/03/18 08:57:58 by seramaro         ###   ########.fr       */
+/*   Created: 2026/03/23 14:12:31 by seramaro          #+#    #+#             */
+/*   Updated: 2026/03/24 01:09:21 by seramaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#define BUFFER_SIZE 5
-#  include <fcntl.h>
-#  include <stddef.h>
-#  include <stdlib.h>
-#  include <unistd.h>
 
-char	*ft_strjoin(char *s1, char *s2);
-
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 07
 # endif
+
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_strlen(char *str);
+int		has_new_line(char *stash);
+char	*append(char *s1, char *s2);
+char	*create_buffer(int fd);
+int		line_size(char *stash);
+void	*ft_memmove(void *dest, const void *src, size_t l);
+char	*create_line(char *stash);
+char	*store_remainder(char *stash);
+void	update_line(char **stash, char **remainder, char **line);
+char	*get_next_line(int fd);
+
+#endif
