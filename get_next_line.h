b3cbea7906125f48e4c0188302368bef45827251
Axl <axlramaroson25@gmail.com>
2026-04-16 10:45:54 +0300

    removed unused functions/libraries declarations

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: seramaro <seramaro@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 14:12:31 by seramaro          #+#    #+#             */
-/*   Updated: 2026/03/25 11:47:25 by seramaro         ###   ########.fr       */
+/*   Updated: 2026/04/16 10:42:15 by seramaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@
 #  define BUFFER_SIZE 07
 # endif
 
-# include <fcntl.h>
 # include <stddef.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -29,8 +27,6 @@ char	*append(char *s1, char *s2);
 char	*create_buffer(int fd);
 int		line_size(char *stash);
 void	*ft_memmove(void *dest, const void *src, size_t l);
-char	*create_line(char *stash);
-char	*store_remains(char *stash);
 void	update_line(char **stash, char **remains, char **line);
 char	*get_next_line(int fd);
 
